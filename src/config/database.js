4d8954dbc569c0c4ef4104335d6bad774a7e1d41
Felipe Augusto Gomes de Oliveira <felipeAgomes>
2020-02-20 18:11:22 -0300

    Básico da interação com o banco

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "fiec@1234",
+  database: "sqlnode",
+  difine: {
+    timestamps: true,
+    underscored: true
+  }
+};
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const UserController = require("./controllers/UserController");
+
+const routes = express.Router();
+
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.store);
+
+module.exports = routes;
